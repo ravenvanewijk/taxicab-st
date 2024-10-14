adc3792cdc088bbefa9179dffaa1c499b2120896
ravenvanewijk <ravenvanewijk1@gmail.com>
2024-10-14 17:44:25 +0200

    ) fix

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", "r") as f:
 
 setup(
     name='taxicab_st',
-    version='1.0.3',
+    version='1.0.4',
     author='Nathan A. Rooy, Raven van Ewijk',
     author_email='nathanrooy@gmail.com, ravenvanewijk1@gmail.com',
     url='https://github.com/ravenvanewijk/taxicab-st',
--- taxicab_st/time.py
+++ taxicab_st/time.py
@@ -339,7 +339,7 @@ def shortest_path(G, orig_yx, dest_yx, orig_edge=None, dest_edge=None):
             if ((nx_route[0], nx_route[1], 0) in G.edges or \
                     (nx_route[0], nx_route[1], 1) in G.edges) and \
                 ((nx_route[-2], nx_route[-1], 0) in G.edges or \
-                    (nx_route[-2], nx_route[-1], 1)) in G.edges:
+                    (nx_route[-2], nx_route[-1], 1) in G.edges):
                 nx_edges = list(route_to_gdf(G, nx_route).index)
             elif (nx_route[0], nx_route[1], 0) not in G.edges and \
                     (nx_route[0], nx_route[1], 1) not in G.edges:
